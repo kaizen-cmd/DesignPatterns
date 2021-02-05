1a21458df4e5b99970a9b725bbbe8db81fd20a5e
Tejas Mandre <tejasmandre0306@gmail.com>
2021-02-05 13:22:27 +0530

    single ton decorator

--- singleton_design.py
+++ singleton_design.py
@@ -1,3 +1,17 @@
+class SingleTonDecorator:
+
+    def __init__(self, clss):
+        self.clss = clss
+        self.instance = None
+
+    def __call__(self, *args, **kwargs):
+        print(self.instance)
+        if self.instance == None:
+            self.instance = self.clss(*args, **kwargs)
+        return self.instance
+
+
+# Normal single Ton
 class President:
 
     def __init__(self, name):
@@ -16,6 +30,26 @@ p = President("Trump")
 q = President("Biden")
 
 print(o, p, q)
+
+print(o.name)
+print(p.name)
+print(q.name)
+
+
+# SignleTon using decorator
+
+@SingleTonDecorator
+class President:
+
+    def __init__(self, name):
+        self.name = name
+
+
+o = President("Obama")
+
+p = President("Trump")
+
+q = President("Biden")
 
 print(o.name)
 print(p.name)
